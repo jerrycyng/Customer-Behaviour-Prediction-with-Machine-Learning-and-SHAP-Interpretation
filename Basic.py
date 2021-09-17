30af51ba517ec138f946dc15616f6a3707c23f34
jerrycyng <68990318+jerrycyng@users.noreply.github.com>
2021-09-17 11:59:48 +0800

    Update and rename SHAP Introduction.py to Basic.py

---
+++ Basic.py
@@ -0,0 +1,16 @@
+# Shapley Values
+
+Assume there's a predictive model have a $\vec{x}\space(x1, x2, x3)$ as the input variables, and $y$ as the corresponding variabe.
+Below is the sample ouput:
+
+| x1 | x2 | x3 | y | $\hat y$ |
+| --- | --- | --- | --- | --- |
+| 1 | 0 | 0 | 5 | 2.5 |
+| 0 | 1 | 0 | 10 | 10 |
+| 0 | 0 | 1 | 15 | 17.5 |
+| 1 | 1 | 0 | 20 | 22.5 |
+| 1 | 0 | 1 | 30 | 30 |
+| 0 | 1 | 1 | 40 | 37.5 |
+| 1 | 1 | 1 | 50 | 50 |
+
+
